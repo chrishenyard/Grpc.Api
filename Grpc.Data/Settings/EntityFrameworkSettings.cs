c23a8a92be9e96ee723a6254c01372201d4ddfc9
Christopher Allen Henyard <chenyard@gmail.com>
2025-12-10 12:16:56 -0700

    Settings validation

--- Grpc.Data/Settings/EntityFrameworkSettings.cs
+++ Grpc.Data/Settings/EntityFrameworkSettings.cs
@@ -1,12 +1,24 @@
-﻿namespace Grpc.Data.Settings;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grpc.Data.Settings;
 
 public class EntityFrameworkSettings
 {
     public const string Section = "EntityFrameworkSettings";
 
+    [Range(1000, 50000)]
     public int BatchSize { get; set; }
+
+    [Range(5, 30)]
     public int CommandTimeout { get; set; }
+
+
+    [Range(1000, 50000)]
     public int BulkRowCount { get; set; }
+
+    [Range(1, 5)]
     public int MaxRetryCount { get; set; }
+
+    [Range(1, 5)]
     public int MaxRetryDelay { get; set; }
 }
--- Grpc.Service/Settings/SeqSettings.cs
+++ Grpc.Service/Settings/SeqSettings.cs
@@ -1,9 +1,16 @@
-﻿namespace Grpc.Service.Settings;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grpc.Service.Settings;
 
 public class SeqSettings
 {
     public const string Section = "SeqSettings";
 
+    [Url]
+    [Required]
     public required string ServerUrl { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 8)]
     public required string ApiKey { get; set; }
 }
